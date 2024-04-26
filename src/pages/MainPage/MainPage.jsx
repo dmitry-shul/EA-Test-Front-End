9b8aeb233dfdd08d406e6f5049c45c5c338e64b6
Dmitry Shulzhytski <matemV@mail.ru>
2024-04-27 00:19:40 +0300

    add background

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,19 @@
+import MainPageBackground from "../../components/MainPageBackground/MainPageBackground"
+import styles from "./MainPage.module.css"
+
+const MainPage = () => {
+  return (
+    <>
+      <div className={styles.main}>
+        <MainPageBackground />
+      </div>
+
+      <div className={styles.footer}>
+        
+      </div>
+    </>
+  )
+}
+
+export default MainPage
+
